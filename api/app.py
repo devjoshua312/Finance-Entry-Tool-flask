0cf736c15581b924cc765c839822d458e565cc20
Esvin Joshua <72260184+devjoshua312@users.noreply.github.com>
2023-07-18 16:43:49 +0530

    foo

--- api/app.py
+++ api/app.py
@@ -12,7 +12,8 @@ LoginManager.session_protection = "strong"
 
 # find out where users.json and funds.json files are located
 # and set the DATA_FOLDER variable to that location
-DATA_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'data')
+DATA_FOLDER = os.path.join(os.path.dirname(
+    os.path.abspath(__file__)), '..', 'data')
 
 
 app = Flask(__name__)
@@ -23,9 +24,11 @@ login_manager.init_app(app)
 
 DATA_FOLDER = os.getcwd()
 
+
 class User(UserMixin):
     def __init__(self, user_id):
         self.id = user_id
+
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -35,7 +38,7 @@ def load_user(user_id):
 users = {
     os.environ.get('USER1'): {
         'password': os.environ.get('PASSWORD1')
-        },
+    },
     os.environ.get('USER2'): {
         'password': os.environ.get('PASSWORD2')
     },
@@ -71,6 +74,7 @@ def save_data(data):
     with open(os.path.join(DATA_FOLDER, 'funds.json'), 'w') as file:
         json.dump(data, file, indent=4)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -91,11 +95,13 @@ def login():
 
     return render_template('login.html')
 
+
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
 
 @app.route('/download_receipt/<donor_name>', methods=['GET'])
 @login_required
@@ -107,6 +113,7 @@ def download_receipt(donor_name):
             if filename.__contains__(donor_name):
                 return send_file(f'receipts/{filename}', as_attachment=True)
 
+
 @app.route('/add_fund', methods=['POST'])
 @login_required
 def add_fund():
@@ -114,7 +121,8 @@ def add_fund():
     name = request.form['name']
     date = request.form['date']
     contact_number = request.form['contact_number']
-    amount_words = num2words(float(request.form['amount_number']), lang='en_IN')
+    amount_words = num2words(
+        float(request.form['amount_number']), lang='en_IN')
     amount_number = request.form['amount_number']
     address = request.form['address']
     if 'receipt' in request.files:
@@ -123,20 +131,19 @@ def add_fund():
     else:
         pass
 
-
-
     if not name or not date or not contact_number or not amount_words or not amount_number:
         return jsonify({'error': 'Please enter all fund details.'})
 
     data = load_data()
 
-    duplicate_fund = next((fund for fund in data.get("Funds", []) if fund['Name'] == name), None)
+    duplicate_fund = next((fund for fund in data.get(
+        "Funds", []) if fund['Name'] == name), None)
 
     if duplicate_fund:
         existing_amount_number = float(duplicate_fund['AmountNumber'])
         new_amount_number = existing_amount_number + float(amount_number)
         duplicate_fund['AmountNumber'] = str(new_amount_number)
-        
+
         new_amount_words = num2words(new_amount_number, lang='en_IN')
         duplicate_fund['AmountWords'] = new_amount_words
     else:
@@ -156,6 +163,7 @@ def add_fund():
 
     return render_template('index.html')
 
+
 @app.route('/remove_donors', methods=['POST'])
 @login_required
 def remove_donors():
@@ -166,12 +174,13 @@ def remove_donors():
 
         data = load_data()
 
-        updated_funds = [fund for fund in data.get("Funds", []) if fund['Name'] != donor_name]
+        updated_funds = [fund for fund in data.get(
+            "Funds", []) if fund['Name'] != donor_name]
         data["Funds"] = updated_funds
 
         save_data(data)
 
-    return(render_template('display_donors.html'))
+    return (render_template('display_donors.html'))
 
 
 @app.route('/display_donors')
@@ -188,7 +197,7 @@ def display_donors():
             if float(fund['AmountNumber']) > highest_amount:
                 highest_amount = float(fund['AmountNumber'])
                 highest_donor = fund['Name']
-        
+
         with open(os.path.join(DATA_FOLDER, 'users.json'), 'r') as file:
             user_data = json.load(file)
         users = user_data.get("users")
@@ -198,6 +207,7 @@ def display_donors():
         return render_template('display_donors.html', funds=[], users=[], username=current_user.id, highest_donor="")
     # finally:
     #     return(f"Aight. looks like you got an error. heres what i know: the data folder is {DATA_FOLDER}. your current dir is {os.getcwd()}. the current user is {current_user.id}. The program couldnt find the json files specified. The files in this directory are: {os.listdir()}")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
