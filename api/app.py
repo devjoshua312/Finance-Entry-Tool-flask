7c129a678101861424a0a9379793663a8c931313
Esvin Joshua <72260184+devjoshua312@users.noreply.github.com>
2023-07-30 11:29:20 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -118,52 +118,59 @@ def download_receipt(donor_name):
 @app.route('/add_fund', methods=['POST'])
 @login_required
 def add_fund():
+    try:
+        
     # Retrieve form data
-    name = request.form['name']
-    date = request.form['date']
-    contact_number = request.form['contact_number']
-    amount_words = num2words(
-        float(request.form['amount_number']), lang='en_IN')
-    amount_number = request.form['amount_number']
-    address = request.form['address']
+        name = request.form['name']
+        date = request.form['date']
+        contact_number = request.form['contact_number']
+        amount_words = num2words(
+              float(request.form['amount_number']), lang='en_IN')
+        amount_number = request.form['amount_number']
+        address = request.form['address']
     # if request.files:
     #     receipt = request.files['receipt']
     #     receipt.save(f'receipts/{name}.{receipt.filename.split(".")[-1]}')
     # else:
     #     pass
 
-    if not name or not date or not contact_number or not amount_words or not amount_number:
-        return jsonify({'error': 'Please enter all fund details.'})
+        if not name or not date or not contact_number or not amount_words or not amount_number:
+             return jsonify({'error': 'Please enter all fund details.'})
 
-    data = load_data()
+        data = load_data()
 
-    duplicate_fund = next((fund for fund in data.get(
-        "Funds", []) if fund['Name'] == name), None)
+        duplicate_fund = next((fund for fund in data.get(
+             "Funds", []) if fund['Name'] == name), None)
 
-    if duplicate_fund:
-        existing_amount_number = float(duplicate_fund['AmountNumber'])
-        new_amount_number = existing_amount_number + float(amount_number)
-        duplicate_fund['AmountNumber'] = str(new_amount_number)
+        if duplicate_fund:
+             existing_amount_number = float(duplicate_fund['AmountNumber'])
+             new_amount_number = existing_amount_number + float(amount_number)
+             duplicate_fund['AmountNumber'] = str(new_amount_number)
 
-        new_amount_words = num2words(new_amount_number, lang='en_IN')
-        duplicate_fund['AmountWords'] = new_amount_words
-    else:
-        new_fund = {
-            "Name": name,
-            "Date": date,
-            "ContactNumber": contact_number,
-            "AmountWords": amount_words,
-            "AmountNumber": amount_number,
-            "Address": address,
-            "type": 'completed transaction'
-        }
+             new_amount_words = num2words(new_amount_number, lang='en_IN')
+             duplicate_fund['AmountWords'] = new_amount_words
+         else:
+             new_fund = {
+                   "Name": name,
+                   "Date": date,
+                   "ContactNumber": contact_number,
+                   "AmountWords": amount_words,
+                   "AmountNumber": amount_number,
+                   "Address": address,
+                   "type": 'completed transaction'
+                 }
 
-        data.setdefault("Funds", []).append(new_fund)
+              data.setdefault("Funds", []).append(new_fund)
 
-    save_data(data)
+           save_data(data)
 
-    return render_template('index.html')
+          return render_template('index.html')
+    except Exception as e:
+        return(e)
+    finally:
+        return(f"Aight. looks like you got an error. heres what i know: the data folder is {DATA_FOLDER}. your current dir is {os.getcwd()}. the current user is {current_user.id}. The program couldnt find the json files specified. The files in this directory are: {os.listdir()}")
 
+     
 
 @app.route('/remove_donors', methods=['POST'])
 @login_required
